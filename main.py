cea7d3ad94f797d45b5d23f795199604831215bb
Konstantinos Karras Kallidromitis <kkarraskallidromitis@gmail.com>
2019-10-26 23:29:43 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ import send_mms
 
 #Parameters
 sender = 'dialogue.cornell@gmail.com'#
-password = "dialogue45@"
+password = "*****" #Placeholder for password
 
 sender_num ='6467278169'
 sender_c = 'tmobile'
-name = "John"
+name = "K"
 
-SMTP_SERVER = "imap.gmail.com" 
+SMTP_SERVER = "imap.gmail.com"
 SMTP_PORT   = 587
 
-carriers = { 'att':'@mms.att.net', 
-    'tmobile':'@tmomail.net', 
-    'verizon':'@vtext.com', 
-    'sprint':'@page.nextel.com', 
+carriers = { 'att':'@mms.att.net',
+    'tmobile':'@tmomail.net',
+    'verizon':'@vtext.com',
+    'sprint':'@page.nextel.com',
     'gmail':'@gmail.com', }
-      
+
 contacts = {sender_c:sender_num}
 
 
@@ -29,10 +29,10 @@ contacts = {sender_c:sender_num}
 #Code
 def read():
     iter=[]
-    mail = imaplib.IMAP4_SSL(SMTP_SERVER) 
-    mail.login(sender,password) 
-    mail.select('inbox') 
-    type, data = mail.search(None,'(FROM +1'+sender_num+'@mailmymobile.net)') 
+    mail = imaplib.IMAP4_SSL(SMTP_SERVER)
+    mail.login(sender,password)
+    mail.select('inbox')
+    type, data = mail.search(None,'(FROM +1'+sender_num+'@mailmymobile.net)')
     for num in data[0].split():
         type, data = mail.fetch(num, '(RFC822)')
         raw=data[0][1].decode('utf-8')
@@ -50,7 +50,7 @@ def read():
             iter.append(result)
             break
     return iter
-    
+
 body = 'server running'
 send_mms.main(sender_c,sender_num,carriers,sender,name,body)
 overall = read()
@@ -64,9 +64,9 @@ while 1:
         if cmd[0][:6]== "Online":
             body="I am available for the next "+cmd[0][6:]+" mins"
             send_mms.main(sender_c,sender_num,carriers,sender,name,body) #self+others
-            #send_mms.main('tmobile','9292505668',carriers,sender,name,body) 
-            send_mms.main('tmobile','828-712-9345',carriers,sender,name,body) 
-            #send_mms.main('att','3326003251',carriers,sender,name,body) 
+            send_mms.main('tmobile','9292505668',carriers,sender,name,body)
+            send_mms.main('tmobile','8287129345',carriers,sender,name,body)
+            send_mms.main('att','3326003251',carriers,sender,name,body)
         elif cmd[0]=="Offline":
             if iter[-2][:6]== "Online":
                 body="I am offline"
